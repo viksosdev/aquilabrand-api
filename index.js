6add657f7e09e2865fc52614994fc751970b2308
VicSoS <vubeda@utem.cl>
2023-05-30 21:55:44 -0400

    yaaa inaaaa

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mysql = require('mysql');
+const fetch = require('node-fetch');
 
 const app = express();
 const port = 3000;
